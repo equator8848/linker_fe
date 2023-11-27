678a8bc926befe965daf9181aae87377f2aa1c26
libinkai <841116931@qq.com>
2023-11-27 08:02:43 +0800

    网站底部文字修改

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -19,7 +19,7 @@
     </div>
     <div id="footer">
       <div id="foot-left">
-        <h5>支持企业私有化部署，4999元协助部署，维保1年</h5>
+        <h4>联系作者</h4>
         <h5>电子邮箱： 841116931@qq.com</h5>
         <img src="../assets/wx.jpg" width="128px" height="128px">
       </div>
